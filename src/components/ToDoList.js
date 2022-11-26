01c48e74c73d8328effd29cd19a87e3e4bc5a824
Bhavit singla <72124587+bhavi-singla@users.noreply.github.com>
2022-11-27 00:21:52 +0530

    Add files via upload

---
+++ src/components/ToDoList.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import TodoItem from './ToDoItem';
+import { useSelector, useDispatch } from 'react-redux';
+
+const TodoList = () => {
+	
+	const dispatch = useDispatch();
+	const todos = useSelector((state) => state.todos);
+
+	
+
+	return (
+		<ul className='list-group'>
+			{todos.map((todo) => (
+				<TodoItem id={todo.id} title={todo.title} completed={todo.completed} />
+			))}
+		</ul>
+	);
+};
+
+export default TodoList;
